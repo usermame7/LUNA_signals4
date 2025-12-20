4aaa4897d422332eaeadb318b66c69777dc5deca
usermame7 <154531526+usermame7@users.noreply.github.com>
2025-12-20 23:10:47 +0200

    Update TG link to +v9nA2WTCO0IxOWVk

--- client/src/pages/ThankYouPage.tsx
+++ client/src/pages/ThankYouPage.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 import { CheckCircle } from "lucide-react";
 
 export default function ThankYouPage() {
-  const telegramUrl = "https://t.me/+aNuif5qjmz8xNzI0";
+  const telegramUrl = "https://t.me/+v9nA2WTCO0IxOWVk";
 
   useEffect(() => {
     // Google Analytics tracking only - NO Lead event here
